37a89442c932b8866376f0ca3a20af0a7170111f
Crowdbotics <team@crowdbotics.com>
2023-12-31 22:35:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "zuma-bet-45237",
+  url: "https://zuma-bet-45237.botics.co",
+  api: "https://zuma-bet-45237.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
